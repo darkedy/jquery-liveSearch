a5ec770949d572c8984b1a76af4a0379298ceea1
Eddie <lordeddie66@gmail.com>
2015-09-20 20:36:59 -0300

    if added

--- search_results.php
+++ search_results.php
@@ -1,19 +1,21 @@
 <?php
 //include("con.php");
 
-$query = mysql_query("SELECT * FROM myTbl WHERE myColumn LIKE '%{$_GET['q']}%'") or die(mysql_error());
-$total = mysql_num_rows($query);
-
-if($total > 0){ 
-    while($reg = mysql_fetch_assoc($query)){
-        echo "<li>",
-                "<a href='#'>",
-                    "<span>Something</span>",
-                    "Something",
-                "</a>",
-             "</li>";
+if($_GET['q'] != ""){
+    $query = mysql_query("SELECT * FROM myTbl WHERE myColumn LIKE '%{$_GET['q']}%'") or die(mysql_error());
+    $total = mysql_num_rows($query);
+    
+    if($total > 0){ 
+        while($reg = mysql_fetch_assoc($query)){
+            echo "<li>",
+                    "<a href='#'>",
+                        "<span>Something</span>",
+                        "Something",
+                    "</a>",
+                 "</li>";
+        }
+    } else {
+        echo "Not Found.";
     }
-} else {
-    echo "Not Found.";
 }
 ?>
